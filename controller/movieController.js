a012efea77dd8bb379c61a1c58c7cfc12c9b5082
Deni0012 <deni.dg00@icloud.com>
2025-05-12 00:43:11 +0200

    feat: finish exercise

--- controller/movieController.js
+++ controller/movieController.js
@@ -2,14 +2,61 @@ const connection = require('../data/movies_db');
 
 function index(req, res) {
 
-    res.send('Index movie');
+    const sql = 'SELECT * FROM movies_db.movies';
+
+    connection.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({
+                errorMessage: "Database disconnected!"
+            });
+
+        };
+
+
+        res.json(results);
+
+
+    });
 
 };
 
 function show(req, res) {
+    const { id } = req.params;
 
-    res.send('Show movie');
+    const sqlMovie = 'SELECT * FROM movies_db.movies WHERE id = ?';
 
+    conn.query(sqlMovie, [id], (err, results) => {
+        if (err) {
+            return res.status(500).json({
+                errorMessage: "Database diconnected!"
+            });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({
+                errorMessage: 'Record Not Found!',
+                id
+            });
+        }
+
+        const movie = results[0];
+
+        const sqlReviews = 'SELECT * FROM movies_db.reviews WHERE movie_id = ?';
+
+        conn.query(sqlReviews, [id], (err, reviewResults) => {
+            if (err) {
+                return res.status(500).json({
+                    errorMessage: "Database diconnected!"
+                });
+            }
+
+            movie.reviews = reviewResults;
+            res.json(movie);
+        });
+    });
 };
+
+
+
 
 module.exports = { index, show };
--- data/movies_db.js
+++ data/movies_db.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'Willy4ever!',
-    database: process.env.DB_NAME || 'movies_db'
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
