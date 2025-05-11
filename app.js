bb36272ccd5f78cb70f55255a82064dc118bffcc
Deni0012 <deni.dg00@icloud.com>
2025-05-11 18:23:32 +0200

    feat: created router and controller

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//routers
+const movieRouter = require('./router/movies');
+
+app.use(express.static('public'));
+
+app.use(express.json());
+
 //middlewares
 const notFound = require('./middlewares/notFound');
 const handleErrors = require('./middlewares/handleErrors');
@@ -10,6 +17,8 @@ const handleErrors = require('./middlewares/handleErrors');
 app.get('/', (req, res) => {
     res.send('Benvenuto nella mia WebApplication');
 })
+
+app.use('/movies', movieRouter);
 
 //use 500
 app.use(handleErrors);
