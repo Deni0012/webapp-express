7b07c254761951aad6d81d05084ba46bd31e3948
Deni0012 <deni.dg00@icloud.com>
2025-05-11 17:24:24 +0200

    feat: created middlewares

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('ciao sono denise');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//middlewares
+const notFound = require('./middlewares/notFound');
+const handleErrors = require('./middlewares/handleErrors');
+
+
+app.get('/', (req, res) => {
+    res.send('Benvenuto nella mia WebApplication');
+})
+
+//use 500
+app.use(handleErrors);
+//use 404
+app.use(notFound);
+
+
+app.listen(port, () => {
+    console.log(`WebApp listening at port ${port}`);
+})
