1a8d17ac72f7d1f27e107c4f53545288007ca544
Deni0012 <deni.dg00@icloud.com>
2025-05-15 15:53:42 +0200

    feat: finish exercise

--- controller/movieController.js
+++ controller/movieController.js
@@ -33,7 +33,19 @@ function index(req, res) {
 function show(req, res) {
     const { id } = req.params;
 
-    const sqlMovie = 'SELECT * FROM movies WHERE id = ?';
+    const sqlMovie = `
+    SELECT
+    movies.*,
+        AVG(reviews.vote) AS avg_vote
+    FROM
+    movies
+        LEFT JOIN 
+            reviews ON movies.id = reviews.movie_id
+    WHERE
+    movies.id = ?
+        GROUP BY
+    movies.id
+    `;
 
     connection.query(sqlMovie, [id], (err, results) => {
         if (err) {
@@ -52,7 +64,7 @@ function show(req, res) {
         const movie = results[0];
         movie.imagepath = process.env.DB_IMG + movie.image;
 
-        const sqlReviews = 'SELECT * FROM movies_db.reviews WHERE movie_id = ?';
+        const sqlReviews = 'SELECT * FROM reviews WHERE movie_id = ?';
 
         connection.query(sqlReviews, [id], (err, reviewResults) => {
             if (err) {
@@ -71,16 +83,59 @@ function starReview(req, res) {
     const { id } = req.params;
     const { text, name, vote } = req.body;
 
-    if (!text || !name || !vote) {
+    if (!text || !name || vote === null || vote === "") {
         return res.status(400).json({
             errorMessage: 'Please fill in all fields!'
         });
     };
 
-    res.send('I added a new review!');
+    const sqlAdd = ` INSERT INTO reviews (movie_id, text, name, vote) 
+    VALUES (?, ?, ?, ?)`
+
+    connection.query(sqlAdd, [id, text, name, vote], (err, addResult) => {
+        if (err) {
+            return res.status(500).json({ errorMessage: "Error saving review!" });
+
+        };
+
+        res.json({
+            id,
+            text,
+            name,
+            vote
+        });
+    });
+
+};
+
+function reviewStore(req, res) {
+    const { title, director, genre, abstract, release_year } = req.body;
+
+    if (!req.file) {
+        return res.status(400).json({ errorMessage: "Missing image!" });
+    }
+
+    const image = req.file.filename;
+
+    const addNewMoviesql = `INSERT INTO movies (title, director, genre, release_year, abstract, image) VALUES (?, ?, ?, ?, ?)`;
+
+    connection.query(addNewMoviesql, [title, director, genre, release_year, abstract, image], (err, result) => {
+        if (err) {
+            return res.status(500).json({ errorMessage: "Error saving movie!" });
+
+        }
+
+        res.status(201).json({
+            message: "Movie saved successfully",
+            movieId: result.insertId
+        });
+    });
 }
 
 
 
 
-module.exports = { index, show, starReview };
+
+
+
+module.exports = { index, show, starReview, reviewStore };
--- router/movies.js
+++ router/movies.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('../middlewares/multer');
 
 const movieController = require('../controller/movieController');
 
 //index;
 router.get('/', movieController.index);
 
-router.post('/id/reviews', movieController.starReview)
+router.post('/:id', movieController.starReview);
+
+router.post('/movies', upload.single('image'), movieController.reviewStore);
 
 //show;
 router.get('/:id', movieController.show);
